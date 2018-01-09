921cfbbd8053e6f731ca5a3ae0fbd70874986d99
sarah lund <sarah.christine.lund@gmail.com>
2018-01-09 16:16:46 -0500

    reseeded db, media query

--- db/seeds/blogs.sql
+++ db/seeds/blogs.sql
@@ -2,7 +2,7 @@ INSERT INTO blog (title, subtitle, img_1, img_2, img_3, issuetime, category, aut
    (
 
     'Millennials Fitness Habits',
-    'Subtitle',
+    'What Millenials look for in their fitness regimes',
     'https://i.imgur.com/dtqcfQp.jpg',
     'https://i.imgur.com/dtqcfQp.jpg',
     'https://i.imgur.com/dtqcfQp.jpg',
@@ -35,9 +35,221 @@ At Prevail Boxing, for instance, cancellations with less than eight hours’ not
 'link 3',
 'link 4',
 'link 5',
-'tag 1',
-'tag 2',
-'tag 3',
-'tag 4',
-'tag 5'
+'Health',
+'Wellness',
+'Fitness',
+'Strength',
+'Holistic'
+  ),
+
+   (
+
+    'Post Flight Immune System Boosters',
+    'How to keep illness at bay by sweating it out',
+    'https://i.imgur.com/fZ26s3a.png',
+    'https://i.imgur.com/pk351zU.png',
+    'https://i.imgur.com/44pv34l.png',
+    'December 16, 2017',
+    'Fitness',
+    'Well + Good',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    'Carla Zuniga is punching a heavy bag as if she were preparing for a title fight, although she’s really a 35-year-old hair stylist doing her regular workout.
+
+Zuniga isn’t sweating at some low-fee, big-box fitness chain. Prevail Boxing is a 1,500-square-foot studio in Los Angeles that charges $250 for 10 classes.',
+
+'“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”
+
+Costly coffee and artisanal avocado toast may be getting the blame for millennials’ inability to afford a house. But those expenses pale compared with what a growing segment is willing to spend on fitness, abandoning $30-a-month gyms for trendy studios where classes for cycling, boot camp or yoga can run $30 a session.',
+
+'Boutique fitness studios have become the only growth segment in an otherwise stagnant gym industry, according to separate research reports from the Assn. of Fitness Studios, fitness technology firm Netpulse and financial services firm Stephens.
+
+“When it comes to the younger generation, consumer items like car and home purchases are at an all-time low,” said Greg Skloot, vice president for growth at Netpulse, a San Francisco company that creates mobile apps for health clubs.',
+
+'“They don’t want an annual gym membership commitment and a contract,” said Skloot, who co-wrote a recent report on fitness industry changes titled “The Club of 2020.” “They want to be able to make physical fitness choices on demand, and they are willing to pay for it.”',
+
+'Spurred by popular startup ClassPass and other online middlemen, young fitness addicts say their days of mindless treadmill workouts tied to just one gym are over. With a limited number of spots per class and advance reservations generally required, there’s a mad rush to get into the hottest classes.
+
+Rules can be strict where studios have waiting lists of a dozen or more people who are hoping someone doesn’t show up for a class so that they can slip into the spot.
+
+At Prevail Boxing, for instance, cancellations with less than eight hours’ notice cost $10 on top of the price of the class. Fail to show and there’s a $20 fee. Arrive less than five minutes early and you stand a chance of losing your class spot to someone else.',
+'link 1',
+'link 2',
+'link 3',
+'link 4',
+'link 5',
+'Health',
+'Wellness',
+'Fitness',
+'Strength',
+'Holistic'
+  ),
+
+   (
+
+    'Show Off Your Earrings',
+    'Frame your face with these beautiful looks',
+    'https://images.contentful.com/g8qtv9gzg47d/5oZqqVvanYgyGioGuGmeCu/27a06649bff2e228e7a73b1d360f7d5c/itg-earrings-26.jpg?fl=progressive&fm=jpg&q=80',
+    'https://images.contentful.com/g8qtv9gzg47d/5HkiZffd5YWAQsMcwkuSEo/88f3ca89385fc31796b27188f42f1bcd/itg-earrings-15.jpg?fl=progressive&fm=jpg&q=80',
+    'https://images.contentful.com/g8qtv9gzg47d/1Fmw1hV3EAg0CqWqocMiaU/41938e1f579a3d031d3c63bb5b48abcc/itg-earrings-1.jpg?fl=progressive&fm=jpg&q=80',
+    'December 30, 2017',
+    'Beauty',
+    'Into the Gloss',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    'Carla Zuniga is punching a heavy bag as if she were preparing for a title fight, although she’s really a 35-year-old hair stylist doing her regular workout.
+
+Zuniga isn’t sweating at some low-fee, big-box fitness chain. Prevail Boxing is a 1,500-square-foot studio in Los Angeles that charges $250 for 10 classes.',
+
+'“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”
+
+Costly coffee and artisanal avocado toast may be getting the blame for millennials’ inability to afford a house. But those expenses pale compared with what a growing segment is willing to spend on fitness, abandoning $30-a-month gyms for trendy studios where classes for cycling, boot camp or yoga can run $30 a session.',
+
+'Boutique fitness studios have become the only growth segment in an otherwise stagnant gym industry, according to separate research reports from the Assn. of Fitness Studios, fitness technology firm Netpulse and financial services firm Stephens.
+
+“When it comes to the younger generation, consumer items like car and home purchases are at an all-time low,” said Greg Skloot, vice president for growth at Netpulse, a San Francisco company that creates mobile apps for health clubs.',
+
+'“They don’t want an annual gym membership commitment and a contract,” said Skloot, who co-wrote a recent report on fitness industry changes titled “The Club of 2020.” “They want to be able to make physical fitness choices on demand, and they are willing to pay for it.”',
+
+'Spurred by popular startup ClassPass and other online middlemen, young fitness addicts say their days of mindless treadmill workouts tied to just one gym are over. With a limited number of spots per class and advance reservations generally required, there’s a mad rush to get into the hottest classes.
+
+Rules can be strict where studios have waiting lists of a dozen or more people who are hoping someone doesn’t show up for a class so that they can slip into the spot.
+
+At Prevail Boxing, for instance, cancellations with less than eight hours’ notice cost $10 on top of the price of the class. Fail to show and there’s a $20 fee. Arrive less than five minutes early and you stand a chance of losing your class spot to someone else.',
+'link 1',
+'link 2',
+'link 3',
+'link 4',
+'link 5',
+'Fashion',
+'Beauty',
+'Trends',
+'',
+''
+  ),
+   (
+
+    'The Best Beauty Secrets of the Year',
+    'Beauty insiders share their best kept secrets',
+    'https://images.contentful.com/g8qtv9gzg47d/5mG3l2Quo8KyQg8Eg2q0Au/25d23c7fbeff604f1eb706c45508c6d8/Sarah-Staudinger-3.jpg?fl=progressive&fm=jpg&q=80',
+    'https://images.contentful.com/g8qtv9gzg47d/6ayecViOoEcka8cSSwSkYk/9165e833783250106de7f6ed05685cae/temple-saint-claire-29.jpg?fl=progressive&fm=jpg&q=80',
+    'https://images.contentful.com/g8qtv9gzg47d/19q7D3yc7SMsuM8cIyQ4Ms/f229a5bacf3dcb047eae60c461099617/ines-rau-1.jpg?fl=progressive&fm=jpg&q=80',
+    'January 8, 2018',
+    'Beauty',
+    'Sarah Lund',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    'Carla Zuniga is punching a heavy bag as if she were preparing for a title fight, although she’s really a 35-year-old hair stylist doing her regular workout.
+
+Zuniga isn’t sweating at some low-fee, big-box fitness chain. Prevail Boxing is a 1,500-square-foot studio in Los Angeles that charges $250 for 10 classes.',
+
+'“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”
+
+Costly coffee and artisanal avocado toast may be getting the blame for millennials’ inability to afford a house. But those expenses pale compared with what a growing segment is willing to spend on fitness, abandoning $30-a-month gyms for trendy studios where classes for cycling, boot camp or yoga can run $30 a session.',
+
+'Boutique fitness studios have become the only growth segment in an otherwise stagnant gym industry, according to separate research reports from the Assn. of Fitness Studios, fitness technology firm Netpulse and financial services firm Stephens.
+
+“When it comes to the younger generation, consumer items like car and home purchases are at an all-time low,” said Greg Skloot, vice president for growth at Netpulse, a San Francisco company that creates mobile apps for health clubs.',
+
+'“They don’t want an annual gym membership commitment and a contract,” said Skloot, who co-wrote a recent report on fitness industry changes titled “The Club of 2020.” “They want to be able to make physical fitness choices on demand, and they are willing to pay for it.”',
+
+'Spurred by popular startup ClassPass and other online middlemen, young fitness addicts say their days of mindless treadmill workouts tied to just one gym are over. With a limited number of spots per class and advance reservations generally required, there’s a mad rush to get into the hottest classes.
+
+Rules can be strict where studios have waiting lists of a dozen or more people who are hoping someone doesn’t show up for a class so that they can slip into the spot.
+
+At Prevail Boxing, for instance, cancellations with less than eight hours’ notice cost $10 on top of the price of the class. Fail to show and there’s a $20 fee. Arrive less than five minutes early and you stand a chance of losing your class spot to someone else.',
+'link 1',
+'link 2',
+'link 3',
+'link 4',
+'link 5',
+'Beauty',
+'Skincare',
+'Hair',
+'Health',
+'Body'
+  ),
+   (
+
+    'Overcoming the Fear of Group Fitness Classes',
+    'The fear we all share- before we learn to love it',
+    'https://classpass.com/blog/wp-content/uploads/2017/10/workout_with_music.png',
+    'https://classpass.com/blog/wp-content/uploads/2017/05/20170518_CP-employees.jpg',
+    'https://classpass.com/blog/wp-content/uploads/2018/01/maybe_you_shouldnt_do_after_class.png',
+    'January 10, 2018',
+    'Health',
+    'The Warm Up',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    'Carla Zuniga is punching a heavy bag as if she were preparing for a title fight, although she’s really a 35-year-old hair stylist doing her regular workout.
+
+Zuniga isn’t sweating at some low-fee, big-box fitness chain. Prevail Boxing is a 1,500-square-foot studio in Los Angeles that charges $250 for 10 classes.',
+
+'“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”
+
+Costly coffee and artisanal avocado toast may be getting the blame for millennials’ inability to afford a house. But those expenses pale compared with what a growing segment is willing to spend on fitness, abandoning $30-a-month gyms for trendy studios where classes for cycling, boot camp or yoga can run $30 a session.',
+
+'Boutique fitness studios have become the only growth segment in an otherwise stagnant gym industry, according to separate research reports from the Assn. of Fitness Studios, fitness technology firm Netpulse and financial services firm Stephens.
+
+“When it comes to the younger generation, consumer items like car and home purchases are at an all-time low,” said Greg Skloot, vice president for growth at Netpulse, a San Francisco company that creates mobile apps for health clubs.',
+
+'“They don’t want an annual gym membership commitment and a contract,” said Skloot, who co-wrote a recent report on fitness industry changes titled “The Club of 2020.” “They want to be able to make physical fitness choices on demand, and they are willing to pay for it.”',
+
+'Spurred by popular startup ClassPass and other online middlemen, young fitness addicts say their days of mindless treadmill workouts tied to just one gym are over. With a limited number of spots per class and advance reservations generally required, there’s a mad rush to get into the hottest classes.
+
+Rules can be strict where studios have waiting lists of a dozen or more people who are hoping someone doesn’t show up for a class so that they can slip into the spot.
+
+At Prevail Boxing, for instance, cancellations with less than eight hours’ notice cost $10 on top of the price of the class. Fail to show and there’s a $20 fee. Arrive less than five minutes early and you stand a chance of losing your class spot to someone else.',
+'link 1',
+'link 2',
+'link 3',
+'link 4',
+'link 5',
+'Health',
+'Wellness',
+'Fitness',
+'Strength',
+'Holistic'
+  ),
+   (
+
+    'How to Meet Your New Best Friend',
+    'Warning: Persperation Required',
+    'https://classpass.com/blog/wp-content/uploads/2017/10/making_friends_in_new_city.png',
+    'https://i.imgur.com/dtqcfQp.jpg',
+    'https://i.imgur.com/dtqcfQp.jpg',
+    'December 14, 2017',
+    'Health',
+    'Sarah Lund',
+    '"We might sit in awe every class of the energy and skills that our instructors have, but at the end of the day, they’re all people just like us. When they ask your feedback after class, don’t hesitate to give it to them. You never know — you might just have more in common with them than a love for fitness."',
+    '“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”',
+    'Carla Zuniga is punching a heavy bag as if she were preparing for a title fight, although she’s really a 35-year-old hair stylist doing her regular workout.
+
+Zuniga isn’t sweating at some low-fee, big-box fitness chain. Prevail Boxing is a 1,500-square-foot studio in Los Angeles that charges $250 for 10 classes.',
+
+'“I think people in my generation are more willing to invest in what challenges them and makes them healthy,” said Zuniga, who grew bored with cheaper, traditional gyms. “It’s expensive to be healthy, but it’s more expensive to be sick.”
+
+Costly coffee and artisanal avocado toast may be getting the blame for millennials’ inability to afford a house. But those expenses pale compared with what a growing segment is willing to spend on fitness, abandoning $30-a-month gyms for trendy studios where classes for cycling, boot camp or yoga can run $30 a session.',
+
+'Boutique fitness studios have become the only growth segment in an otherwise stagnant gym industry, according to separate research reports from the Assn. of Fitness Studios, fitness technology firm Netpulse and financial services firm Stephens.
+
+“When it comes to the younger generation, consumer items like car and home purchases are at an all-time low,” said Greg Skloot, vice president for growth at Netpulse, a San Francisco company that creates mobile apps for health clubs.',
+
+'“They don’t want an annual gym membership commitment and a contract,” said Skloot, who co-wrote a recent report on fitness industry changes titled “The Club of 2020.” “They want to be able to make physical fitness choices on demand, and they are willing to pay for it.”',
+
+'Spurred by popular startup ClassPass and other online middlemen, young fitness addicts say their days of mindless treadmill workouts tied to just one gym are over. With a limited number of spots per class and advance reservations generally required, there’s a mad rush to get into the hottest classes.
+
+Rules can be strict where studios have waiting lists of a dozen or more people who are hoping someone doesn’t show up for a class so that they can slip into the spot.
+
+At Prevail Boxing, for instance, cancellations with less than eight hours’ notice cost $10 on top of the price of the class. Fail to show and there’s a $20 fee. Arrive less than five minutes early and you stand a chance of losing your class spot to someone else.',
+'link 1',
+'link 2',
+'link 3',
+'link 4',
+'link 5',
+'Health',
+'Wellness',
+'Fitness',
+'Strength',
+'Holistic'
   );
